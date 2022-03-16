0c55fad8692d93378bac54832f40104d87b531cd
Ashwani Sharma <ashwani.sharma@rimus-tech.com>
2022-03-16 13:49:15 +0530

    removed unused code

--- src/components/molecules/ListItem/index.js
+++ src/components/molecules/ListItem/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
 const ListItem = (props) =>{
     const onClickHandler = () => {
         props.onSelected(props.player);
