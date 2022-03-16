306a3bbd6d9bf0e713c46614da7337cc8137b1f9
Ashwani Sharma <ashwani.sharma@rimus-tech.com>
2022-03-16 13:56:17 +0530

    updated http to https

--- src/api/APIEndPoints.js
+++ src/api/APIEndPoints.js
@@ -1,2 +1,2 @@
-export const END_POINT_TEAMS_URL = `http://api.football-data.org/v2/competitions/2021/teams/`;
-export const END_POINT_PLAYERS_URL = `http://api.football-data.org/v2/teams/`
+export const END_POINT_TEAMS_URL = `https://api.football-data.org/v2/competitions/2021/teams/`;
+export const END_POINT_PLAYERS_URL = `https://api.football-data.org/v2/teams/`
